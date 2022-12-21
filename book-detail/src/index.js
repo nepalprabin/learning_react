9eb66356a585feeb0c6632248002fc204f98c89b
Prabin Nepal <prabinnepal1996@gmail.com>
2022-12-21 12:30:09 -0600

    Added css file

--- book-detail/src/index.js
+++ book-detail/src/index.js
@@ -1,14 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// CSS
+import './index.css';
+
 // stateless functional component
 // always returns JSX
 function BookList() {
   return (
-    <div>
-      <h1>This is a Book List interface (like a clone of Amazon book detail page.</h1>
-    </div>
+    <section className="booklist">
+      <Book />
+    </section>
   );
+}
+
+const Book = () => {
+  return (<div>
+    <article className="book">
+      <img src="https://m.media-amazon.com/images/I/81-lFNjZorL._AC_UL640_FMwebp_QL65_.jpg" alt="" />
+      <h3>I'm Glad My Mom Died</h3>
+      <h4>Jennette McCurdy</h4>
+    </article>
+  </div>)
 }
 
 ReactDOM.render(<BookList />, document.getElementById("root"));

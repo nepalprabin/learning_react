2fa04f7912710a8fcb2ca61a2584b0d109a3bb84
Prabin Nepal <prabinnepal1996@gmail.com>
2022-12-21 13:32:53 -0600

    Accessing data from lists

--- book-detail/src/index.js
+++ book-detail/src/index.js
@@ -7,45 +7,38 @@ import './index.css';
 // stateless functional component
 // always returns JSX
 
-const firstBook = {
-  img: "https://m.media-amazon.com/images/I/81-lFNjZorL._AC_UL640_FMwebp_QL65_.jpg",
-  title: "I'm Glad My Mom Died",
-  author: "Jennette McCurdy"
-}
+const books = [
+    {
+    img: "https://m.media-amazon.com/images/I/81-lFNjZorL._AC_UL640_FMwebp_QL65_.jpg",
+    title: "I'm Glad My Mom Died",
+    author: "Jennette McCurdy"
+    },
+    {
+      img: "https://m.media-amazon.com/images/I/91+NmjDsLOL._AC_UL640_FMwebp_QL65_.jpg",
+      title: "Regretting You",
+      author: "Colleen Hoover"
+    } 
+  ]
 
-const secondBook = {
-  img: "https://m.media-amazon.com/images/I/91+NmjDsLOL._AC_UL640_FMwebp_QL65_.jpg",
-  title: "Regretting You",
-  author: "Colleen Hoover"
-}
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book 
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>Beloved #1 bestselling author James Patterson delivers a thrilling novel about a teen caught between two worlds and the truths that could set her free—or trap her forever</p>
-      </Book>
-
-      <Book 
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return (<Book book={book}></Book>)
+      })
+    }
     </section>
   );
 }
 
 const Book = (props) => {
+  const {img, title, author} = props.book
   return (<div>
     <article className="book">
-      <img src={props.img} alt="" />
-      <h3>{props.title}</h3>
-      <h4>{props.author}</h4>
-      {props.children}
+      <img src={img} alt="" />
+      <h3>{title}</h3>
+      <h4>{author}</h4>
     </article>
   </div>)
 }

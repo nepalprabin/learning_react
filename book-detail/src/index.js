37a370b0458445f0630bc8878915e231dc9c5967
Prabin Nepal <prabinnepal1996@gmail.com>
2022-12-21 12:50:34 -0600

    Props

--- book-detail/src/index.js
+++ book-detail/src/index.js
@@ -6,20 +6,43 @@ import './index.css';
 
 // stateless functional component
 // always returns JSX
+
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/81-lFNjZorL._AC_UL640_FMwebp_QL65_.jpg",
+  title: "I'm Glad My Mom Died",
+  author: "Jennette McCurdy"
+}
+
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/91+NmjDsLOL._AC_UL640_FMwebp_QL65_.jpg",
+  title: "Regretting You",
+  author: "Colleen Hoover"
+}
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book 
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+
+      <Book 
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
   return (<div>
     <article className="book">
-      <img src="https://m.media-amazon.com/images/I/81-lFNjZorL._AC_UL640_FMwebp_QL65_.jpg" alt="" />
-      <h3>I'm Glad My Mom Died</h3>
-      <h4>Jennette McCurdy</h4>
+      <img src={props.img} alt="" />
+      <h3>{props.title}</h3>
+      <h4>{props.author}</h4>
     </article>
   </div>)
 }
